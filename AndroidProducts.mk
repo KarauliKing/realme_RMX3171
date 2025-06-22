38e91379f99f8fdfc683edb7483ad868efc12926
karauli01king <130498383+KarauliKing@users.noreply.github.com>
2025-06-23 04:23:51 +0530

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_RMX3171.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_RMX3171-user \
+    lineage_RMX3171-userdebug \
+    lineage_RMX3171-eng
---
+++ Makefile
@@ -0,0 +1,6 @@
+# Makefile for Realme Narzo 30A (RMX3171)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+include $(call all-makefiles-under,$(LOCAL_PATH))
