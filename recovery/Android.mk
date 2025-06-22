d35b1ab65e4c230f5e0736ee8cb80a3d964daa00
karauli01king <130498383+KarauliKing@users.noreply.github.com>
2025-06-23 03:58:43 +0530

    Add files via upload

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,1 +1,6 @@
-
+// Recovery Android.mk for RMX3171
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := recovery_rmz3171
+LOCAL_SRC_FILES := recovery.img
+include $(BUILD_PREBUILT)
